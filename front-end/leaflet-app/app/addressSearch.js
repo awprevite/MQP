97aea5982f4f8497bdf4c1066c6cf8d52b7aa7d6
awprevite <awprevite@gmail.com>
2025-04-02 17:33:20 -0400

    Location services, updated UI

---
+++ front-end/leaflet-app/app/addressSearch.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./globals.css";
+
+const AddressSearch = ({ className, setUserCoordinates }) => {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [debounceTimer, setDebounceTimer] = useState(null);
+
+  // Fetch autocomplete suggestions
+  const fetchSuggestions = (address) => {
+    const api = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}&addressdetails=1&limit=5&countrycodes=us`;
+
+    axios
+      .get(api)
+      .then((response) => {
+
+        const filteredResults = response.data.filter(item => {
+          const city = item.address?.city;
+          const state = item.address?.state;
+          return city === 'Worcester' && state === 'Massachusetts';
+        });
+
+        if (filteredResults.length > 0) {
+          setSuggestions(filteredResults);
+        } else {
+          setSuggestions([]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching suggestions:", error);
+      });
+  };
+
+  // Handle user input with debounce
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    // If the input length is greater than 2
+    if (value.length > 2) {
+      // Clear the previous timeout if user types again before 3 seconds
+      if (debounceTimer) {
+        clearTimeout(debounceTimer);
+      }
+
+      // Set a new timeout to fetch suggestions after 3 seconds after the last input change
+      const timer = setTimeout(() => {
+        fetchSuggestions(value);
+      }, 3000); // 3 seconds delay
+
+      setDebounceTimer(timer); // Store the timer to clear it later
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  // Handle selecting an address from suggestions
+  const handleSelect = (address) => {
+    setQuery(address.display_name);
+    setSuggestions([]); // Hide suggestions
+
+    // Move map to selected address
+    setUserCoordinates({ lat: address.lat, lng: address.lon });
+  };
+
+  return (
+    <div>
+      <input
+        className={className}
+        type='text'
+        value={query}
+        onChange={handleInputChange}
+        placeholder={`search for ${className === "origin-input" ? "origin" : "destination"} address`}
+      />
+
+      {/* Autocomplete dropdown */}
+      {suggestions.length > 0 && (
+        <ul className={className === "origin-input" ? "origin-ul" : "destination-ul"}>
+          {suggestions.map((item) => (
+            <li 
+              key={item.place_id} 
+              onClick={() => handleSelect(item)}
+              style={{ cursor: "pointer", padding: "5px" }}
+            >
+              {item.display_name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default AddressSearch;
--- front-end/leaflet-app/app/page.js
+++ front-end/leaflet-app/app/page.js
@@ -1,50 +1,45 @@
 'use client'
 
-import React, { useState, useEffect, useRef } from "react";
-import dynamic from "next/dynamic"; // Import dynamic from next
-import { Marker, Tooltip, useMapEvents } from "react-leaflet";
+import React, { useState, useRef } from "react";
+import dynamic from "next/dynamic";
+import { Marker, useMapEvents } from "react-leaflet";
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
 import "./globals.css"
 import L from "leaflet";
+import AddressSearch from "./addressSearch";
+import { Navigation } from "lucide-react";
 
-// Dynamically import MapContainer and GeoJSON components, disabling SSR
+// Dynamically import MapContainer and GeoJSON components, disabling SSR, Leaflet does not work with SSR
 const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), { ssr: false });
 const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), { ssr: false });
 const GeoJSON = dynamic(() => import('react-leaflet').then((mod) => mod.GeoJSON), { ssr: false });
 
-const customIcon = L.divIcon({
+const customIconDestination = L.divIcon({
   className: '',
-  html: '<div class=outer-circle><div class="inner-circle"></div></div>',
+  html: '<div class=outer-circle><div class="inner-circle-white"></div></div>',
+  iconSize: [20, 20],
+  iconAnchor: [10, 10],
+})
+
+const customIconOrigin = L.divIcon({
+  className: '',
+  html: '<div class=outer-circle><div class="inner-circle-green"></div></div>',
   iconSize: [20, 20],
   iconAnchor: [10, 10],
 })
 
 export default function Home() {
-  
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const DefaultIcon = L.icon({
-        iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-      });
-      L.Marker.prototype.options.icon = DefaultIcon;
-    }
-  }, []); 
 
-  const MARKER_START = 0;
-  const MARKER_END = 1;
+  const ORIGIN_MARKER = 0;
+  const DESTINATION_MARKER = 1;
 
-  const [startMarker, setStartMarker] = useState(null);
-  const [endMarker, setEndMarker] = useState(null);
-  const [currentMarker, setCurrentMarker] = useState(MARKER_START);
+  const [originCoordinates, setOriginCoordinates] = useState(null);
+  const [destinationCoordinates, setDestinationCoordinates] = useState(null);
+  const [currentMarker, setCurrentMarker] = useState(ORIGIN_MARKER);
 
   const [loading, setLoading] = useState(false);
   const [time, setTime] = useState("");
-
-  const [userCoordinates, setUserCoordinates] = useState(null);
 
   const [geojsonData, setGeojsonData] = useState(null);
   const coolShapeLength= useRef(0);
@@ -55,8 +50,6 @@ export default function Home() {
   const formatter = new Intl.NumberFormat('en-US', {
     maximumFractionDigits: 2,  // Specify the number of decimal places
   });
-
-  const[showMetric, setShowMetric] = useState(false);
 
   const geojsonStyle = {
     color: "green",
@@ -76,7 +69,7 @@ export default function Home() {
       (position) => {
         const { latitude, longitude } = position.coords;
         if (latitude && longitude) {
-          setUserCoordinates({ lat: latitude, lng: longitude });
+          setOriginCoordinates({ lat: latitude, lng: longitude });
         } else {
           alert("Unable to parse coordinates");
         }
@@ -88,7 +81,6 @@ export default function Home() {
     );
   };
   
-
   const polygon = L.polygon([
     [42.20, -71.90],  // Southwest corner, just outside Worcester to the west
     [42.20, -71.70],  // Southeast corner, just outside Worcester to the east
@@ -106,47 +98,43 @@ export default function Home() {
 
         if (polygon.getBounds().contains(point)) {
 
-          if (currentMarker === MARKER_START) {
-            setStartMarker({ lat, lng });
-            //setStartCoordinateText(`Start: ${lat.toFixed(6)}, ${lng.toFixed(6)}`);
+          if (currentMarker === ORIGIN_MARKER) {
+            setOriginCoordinates({ lat, lng });
           } else {
-            setEndMarker({ lat, lng });
-            //setEndCoordinateText(`End: ${lat.toFixed(6)}, ${lng.toFixed(6)}`);
+            setDestinationCoordinates({ lat, lng });
           }
         }else{
           console.log("clicked too far away")
         }
       },
     });
-    return null;
   };
 
   // Toggle marker between start and end
   const toggleMarker = (which) => {
     if (which === "start") {
-      setCurrentMarker(MARKER_START);
+      setCurrentMarker(ORIGIN_MARKER);
     } else {
-      setCurrentMarker(MARKER_END);
+      setCurrentMarker(DESTINATION_MARKER);
     }
   };
 
-  const toggleMarkerStart = () => (currentMarker === MARKER_START ? "selected" : "regular");
-  const toggleMarkerEnd = () => (currentMarker === MARKER_END ? "selected" : "regular");
+  const toggleMarkerStart = () => (currentMarker === ORIGIN_MARKER ? "selected" : "regular");
+  const toggleMarkerEnd = () => (currentMarker === DESTINATION_MARKER ? "selected" : "regular");
 
   const sendCoordinatesToAPI = (e) => {
 
-    if (!startMarker || !endMarker || time === "") {
+    if (!originCoordinates || !destinationCoordinates || time === "") {
       console.log("Both markers need to be placed on the map and a time must be entered before sending.");
       return;
     }
 
     setLoading(true);
 
-    const coordinates = { start: startMarker, end: endMarker, time: time };
+    const coordinates = { start: originCoordinates, end: destinationCoordinates, time: time };
     console.log(coordinates);
 
-    axios
-      .post("http://127.0.0.1:5000/temp", coordinates)
+    axios.post("http://127.0.0.1:5000/temp", coordinates)
       .then((response) => {
         console.log("Data received successfully:", response.data);
         if (response.data.direct_route && response.data.direct_route.type === "FeatureCollection") {
@@ -175,37 +163,13 @@ export default function Home() {
   };
 
   return (
-    <div className='vertical-container'>
+    <>
       <div className='map-container'>
-        <MapContainer center={[42.2626, -71.8079]} zoom={13} style={{ height: "100%", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://carto.com/attributions">CartoDB</a>'
-          />
-          <GeoJSON key={`0-${directGeojsonData ? JSON.stringify(directGeojsonData) : 'empty'}`} data={directGeojsonData} style={directGeojsonStyle}/>
-          <GeoJSON key={`1-${geojsonData ? JSON.stringify(geojsonData) : 'empty'}`} data={geojsonData} style={geojsonStyle}/>
-          {userCoordinates && <Marker icon={customIcon} position={[userCoordinates.lat, userCoordinates.lng]}></Marker>}
-          <ClickHandler />
-          {startMarker && (
-            <Marker icon={customIcon} position={[startMarker.lat, startMarker.lng]}></Marker>
-          )}
-          {endMarker && (
-            <Marker icon={customIcon} position={[endMarker.lat, endMarker.lng]}></Marker>
-          )}
-        </MapContainer>
-      </div>
-      <div className='button-container'>
-        <div className='horizontal-container'>
-          <label>Direct Distance: {directShapeLength.current} Mi</label>
-          <label>Cool Distance: {coolShapeLength.current} Mi</label>
-        </div>
-        <div className='horizontal-container'>
-          <input type='text' placeholder='Origin'/>
-          <button onClick={findLocation}>Locate</button>
-        </div>
-        <div className='horizontal-container'>
-          <input type='text' placeholder='Destination'/>
-          <select className='dropdown' value={time} onChange={(e) => setTime(e.target.value)}>
+        <button className={`set-origin-button ${toggleMarkerStart()}`} onClick={() => toggleMarker("start")}>Set Origin</button>
+        <button className={`set-destination-button ${toggleMarkerEnd()}`} onClick={() => toggleMarker("end")}>Set Destination</button>
+        <button className='calculate-button' onClick={sendCoordinatesToAPI}>Calculate Route</button>
+        <button className='locate-button' onClick={findLocation}><Navigation size={18} /></button>
+        <select className='time-dropdown' value={time} onChange={(e) => setTime(e.target.value)}>
             <option value="" disabled>Select a time</option>
             <option value="6">6 am or earlier</option>
             <option value="7">7 am </option>
@@ -223,18 +187,27 @@ export default function Home() {
             <option value="19">7 pm</option>
             <option value="20">8 pm or later</option>
           </select>
-        </div>
-        <div className='horizontal-container'>
-          <button className={toggleMarkerStart()} onClick={() => toggleMarker("start")}>Set Start</button>
-          <button className={toggleMarkerEnd()} onClick={() => toggleMarker("end")}>Set End</button>
-          <button onClick={sendCoordinatesToAPI}>Calculate Route</button>
-        </div>
-        <div className='loading-modal'>
-          {loading &&
-            <div className='loader'></div>
-          }
-        </div>
+        <AddressSearch className='origin-input' setUserCoordinates={setOriginCoordinates} />
+        <AddressSearch className='destination-input' setUserCoordinates={setDestinationCoordinates} />
+        <MapContainer center={[42.2626, -71.8079]} zoom={13} style={{ height: "100%", width: "100%"} } zoomControl={false}>
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://carto.com/attributions">CartoDB</a>'
+          />
+          <GeoJSON key={`0-${directGeojsonData ? JSON.stringify(directGeojsonData) : 'empty'}`} data={directGeojsonData} style={directGeojsonStyle}/>
+          <GeoJSON key={`1-${geojsonData ? JSON.stringify(geojsonData) : 'empty'}`} data={geojsonData} style={geojsonStyle}/>
+          <ClickHandler />
+          {originCoordinates && (<Marker icon={customIconOrigin} position={[originCoordinates.lat, originCoordinates.lng]}></Marker>)}
+          {destinationCoordinates && (<Marker icon={customIconDestination} position={[destinationCoordinates.lat, destinationCoordinates.lng]}></Marker>)}
+        </MapContainer>
+        <label className='direct-distance'>Direct Distance: {directShapeLength.current} Mi</label>
+        <label className='cool-distance'>Cool Distance: {coolShapeLength.current} Mi</label>
       </div>
-    </div>
+      <div className='loading-modal'>
+        {loading &&
+          <div className='loader'></div>
+        }
+      </div>
+    </>
   );
 }
